1686f7b081aad8c5ae9630e3918c850b0ee07e60
devanathan123 <60214080+devanathan123@users.noreply.github.com>
2024-04-13 18:26:00 +0530

    Update login.py

--- login.py
+++ login.py
@@ -2,11 +2,25 @@ import streamlit as st
 import firebase_admin
 from firebase_admin import credentials, auth
 from firebase_admin import exceptions as firebase_exceptions
+import requests
+
+# Load JSON key file from GitHub
+def load_json_key():
+    url = "https://raw.githubusercontent.com/devanathan123/stream_app/main/private_key.json"
+    try:
+        response = requests.get(url)
+        response.raise_for_status()  # Raise an error for HTTP errors (e.g., 404, 500)
+        key_data = response.json()
+        return key_data
+    except Exception as e:
+        st.error(f"Error fetching JSON key: {e}")
+        return None
 
 # Check if Firebase Admin SDK is already initialized
 if not firebase_admin._apps:
     # Initialize Firebase Admin SDK
-    cred = credentials.Certificate(r"C:\Users\HP\PycharmProjects\stream\josn_key\private_key.json")
+    key_data = load_json_key()
+    cred = credentials.Certificate(key_data)
     firebase_admin.initialize_app(cred)
 
 
