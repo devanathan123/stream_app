eaaddf2a6e720826625df77a6870886f4a712daf
devanathan123 <60214080+devanathan123@users.noreply.github.com>
2024-04-15 22:08:54 +0530

    Update video.py

--- video.py
+++ video.py
@@ -3,9 +3,9 @@ import streamlit as st
 def main():
     st.title("Streamlit Video Player")
 
-    #Google Drive video URL
-    video_url = "https://firebasestorage.googleapis.com/v0/b/fir-ec695.appspot.com/o/t-15%2Finput_side.mp4?alt=media&token=0cf29a63-b98b-451a-a9f5-7e2d71caf9c3"  # Replace "your-file-id" with the actual file ID
-
+    #Google Drive video URL VIDEO .mp4
+    #video_url = "https://firebasestorage.googleapis.com/v0/b/fir-ec695.appspot.com/o/t-15%2Finput_side.mp4?alt=media&token=0cf29a63-b98b-451a-a9f5-7e2d71caf9c3"  # Replace "your-file-id" with the actual file ID
+    video_url = "https://drive.google.com/file/d/1gurNIOzlaJ76GsWHXcSz8Ua-bKYkllcw/view?usp=drive_link"
     #Display the video
     st.video(video_url)
 
